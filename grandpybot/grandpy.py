df805b47304731cd0b22642243bac66091d806af
loictessier <loictessier.inge@gmail.com>
2018-05-30 20:29:54 +0200

    Iterate grandpy.py

--- grandpybot/grandpy.py
+++ grandpybot/grandpy.py
@@ -8,48 +8,61 @@ INTERROGATION_TERMS = ["que", "où", "comment", "à", "quel", "quelle", "demande
 
 
 class Grandpy:
+    """
+        GrandpyBot
+    """
 
     def __init__(self):
         self.parser = Parser()
 
     def grandpy_answer(self, user_raw_text):
-        # get question out of the raw_text
+        """
+            Grandpy try to answer the user by looking for the question terms
+            then finding the address corresponding and informations about it
+        """
         question = self._search_question(user_raw_text)
-        # parse question to extract keywords
         keywords = self.parser.get_keywords(question)
-        # search for an adress based on keyword
         address = search_address(keywords)
-        # if an address is found search for informations about the location
         if address is not None:
             extract = self._get_extract(address)
         return self._format_grandpy_answer(address, extract)
 
     def _search_question(self, raw_text):
-        # use parser to get sentences from raw_text
+        """
+            Try to get the first question out of a raw text and return it
+        """
         sentences = Parser.raw_to_sentences(raw_text)
-        # extract the question from the sentence
         for s in sentences:
             if self._is_question(s):
                 return s
         return None
 
     def _is_question(sentence):
-        # check for interrogative terms
+        """
+            Return True if the sentence passed as parameter is considered
+            as a question
+        """
         for word in sentence.split():
             if any(fuzz.ratio(word, term) >= 90 for term in INTERROGATION_TERMS):
                 return True
         return False
 
     def _get_extract(self, address):
-        # extract road name from address
+        """
+            Try to get informations (extract) about the road name of the
+            address passed as parameter and return it
+        """
         road = filter(lambda x: x.isalpha(), adress.split(',')[0])
         road_name = ''.join([i for i in road if not i.isdigit()]).strip()
-        # call search_mediawiki on the road name to get text
         extract = search_mediawiki(road_name)
-        # returns formatted text as a message from grandpy
         return extract
 
     def _format_grandpy_answer(self, address, extract):
-        # returns grandpy answer as json object 
+        """
+            Return all elements of Grandpy answer as a JSON Object
+        """
         # ajouter lien page https://fr.wikipedia.org/wiki?curid=5653202
-        pass
+        answer = {}
+        answer['address'] = address
+        answer['extract'] = extract
+        return json.dumps(answer)
